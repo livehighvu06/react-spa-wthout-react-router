308b1a1adb431a0712a53a7b22417ef831a1162e
Eddie Chen <livehighvu06@gmail.com>
2023-02-07 12:46:08 +0800

    調整環境變數

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 const App = () => {
+  let pathTo = "";
+  if (process.env.NODE_ENV !== "production") {
+    pathTo = process.env.REACT_APP_TEST_PATH;
+  } else {
+    pathTo = process.env.REACT_APP_PATH;
+  }
   return (
     <div>
-      <Link to="/react-spa-wthout-react-router/">Home</Link>
-      <Link to="/react-spa-wthout-react-router/about">About</Link>
-      <Link to="/react-spa-wthout-react-router/contact">Contact</Link>
+      <Link to={pathTo}>Home</Link>
+      <Link to={`${pathTo}about`}>About</Link>
+      <Link to={`${pathTo}contact`}>Contact</Link>
 
       <div>
-        <Route path="/react-spa-wthout-react-router/">
+        <Route path={pathTo}>
           <Home />
         </Route>
-        <Route path="/react-spa-wthout-react-router/about">
+        <Route path={`${pathTo}about`}>
           <About />
         </Route>
-        <Route path="/react-spa-wthout-react-router/contact">
+        <Route path={`${pathTo}contact`}>
           <Contact />
         </Route>
       </div>
